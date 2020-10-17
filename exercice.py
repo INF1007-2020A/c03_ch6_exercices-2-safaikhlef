2416630730f451152c4a3442ace6a4165132feaa
safaikhlef <71021589+safaikhlef@users.noreply.github.com>
2020-10-16 21:52:21 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -11,21 +11,9 @@ def reverse_data(data: list = None):
     # et les retourner en ordre inverse, sans utiliser de liste.
 
     if data is None:
-        lifo = LifoQueue()
-        for element in range (10):
-            lifo.put(float(input("Entrez une valeur.")))
+       pass
             
-        #liste = [lifo.pop() for _ in range(lifo.qsize())], qsize pas fiable si plusieurs personnes en même temps, mais si tout seul très bien, prend la longueur au début, si quelqu'un ajoute un élément entre temps je ne viderais pas complètement la pile
-        liste = []
-        while not lifo.empty():
-            liste.append(lifo.get())
-            
-        liste_trie = sorted(liste)
-        for element in liste_trie:
-            lifo.put(element)
-            
-
-    reversed_data = lifo  # Stocker le résultat ici
+    reversed_data = None  # Stocker le résultat ici
 
     return reversed_data
 
@@ -42,6 +30,58 @@ def delete_nth_from_queue(data: Queue, position: int) -> Queue:
 
 def sort_stack(data: Stack) -> Stack:
     # TODO: Retourner la séquence triée
+    
+    ma_liste = [1,6,5,8]
+     lifo = LifoQueue()
+        for element in ma_liste:
+            lifo.put(element)
+            
+        #liste = [lifo.pop() for _ in range(lifo.qsize())], qsize pas fiable si plusieurs personnes en même temps, mais si tout seul très bien, prend la longueur au début, si quelqu'un ajoute un élément entre temps je ne viderais pas complètement la pile
+        liste = []
+        while not lifo.empty():
+            liste.append(lifo.get())
+            
+        liste_trie = sorted(liste)
+        for element in liste_trie:
+            lifo.put(element)
+            
+   """ ajout d'un nouvel élément dans notre pile triée
+    avec une liste
+    variable = 5
+    
+    liste_tmp = []
+    while not lifo.empty():
+            current = lifo.get()
+            if current <= variable:
+                lifo.put(current)
+                lifo.put(variable)
+                break
+            else:
+                liste_tmp.append(lifo.get())
+    for element in reversed(liste_tmp):
+        lifo.put(element)
+        
+     while not lifo.empty(): pour afficher
+        print(lifo.get())
+        
+    avec une autre pile
+    variable = 5
+    
+    autre_pile = LifoQueue()
+    while not lifo.empty():
+            current = lifo.get()
+            if current <= variable:
+                lifo.put(current)
+                lifo.put(variable)
+                break
+            else:
+                autre_pile.put(lifo.get())
+                
+    while not autre_pile.empty():
+        lifo.put(autre_pile.get())
+        
+     while not lifo.empty(): pour afficher
+        print(lifo.get())"""
     return Stack()
 
 
